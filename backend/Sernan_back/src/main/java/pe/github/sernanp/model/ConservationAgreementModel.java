fb41208075f265948b3a62eb0b69c4af7c1ea56d
Pier Rivera <pier.rivera9@gmail.com>
2021-08-14 02:30:15 -0500

    merge ca

--- backend/Sernan_back/src/main/java/pe/github/sernanp/model/ConservationAgreementModel.java
+++ backend/Sernan_back/src/main/java/pe/github/sernanp/model/ConservationAgreementModel.java
@@ -44,10 +44,6 @@ public class ConservationAgreementModel extends BaseModel {
 		this.Name = name;
 	}
 
-	public int getId() {
-		return id;
-	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
