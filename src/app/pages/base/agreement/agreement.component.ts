251d5cf4ebb90a0c433e3bec4e4cca60d9460fc9
Ernesto Tapia <ernesto.tapia@touchmobile.pe>
2021-08-14 16:05:01 -0500

    1

--- src/app/pages/base/agreement/agreement.component.ts
+++ src/app/pages/base/agreement/agreement.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import ogr2ogr from 'ogr2ogr';
 
 @Component({
   selector: 'app-agreement',
@@ -9,4 +10,35 @@ export class AgreementComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+  readURL(event): void {
+    // this.validateSize = false;
+    if (event.target.files[0] && event.target.files) {
+      var file = event.target.files[0];
+      console.log(file);
+      console.log(file.size);
+      // let shape = new FileReader();
+      // shape.readAsBinaryString(file);
+
+      // if (file.size < 50000) {
+
+      //   const reader = new FileReader();
+      //   reader.onload = e => this.imageSrc = reader.result;
+
+      //   reader.readAsDataURL(file);
+      //   this.spinner.show();
+      //   this.datosPersonalesService.datosPersonalUploadPhoto(file, this.token.access_token)
+      //     .subscribe(data => {
+      //       this.updateUrlFoto(data);
+      //     });
+      // } else {
+      //   this.validateSize = true;
+      // }
+    }
+
+    // this.onRefreshPostulanteResumen();
+  }
+  async convertToGeoJson(file) {
+    // let data = await ogr2ogr(file);
+    // console.log(data);
+  }
 }
