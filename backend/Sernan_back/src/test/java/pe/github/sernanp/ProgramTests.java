3131b1278d6b0bbad98ef9c4705fff3af5c41aaf
Pier Rivera <pier.rivera9@gmail.com>
2021-08-16 13:07:53 -0500

    Test

--- backend/Sernan_back/src/test/java/pe/github/sernanp/ProgramTests.java
+++ backend/Sernan_back/src/test/java/pe/github/sernanp/ProgramTests.java
@@ -1,10 +1,9 @@
-package com.example.demo;
-
+package pe.github.sernanp;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestConnectPostgresqlApplicationTests {
+class ProgramTests {
 
 	@org.junit.Test
 	void contextLoads() {
