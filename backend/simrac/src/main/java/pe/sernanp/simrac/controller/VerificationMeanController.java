a5f7144298e089a0dec29bfcaf4af451175c4105
Pier Rivera <pier.rivera9@gmail.com>
2021-08-17 01:14:39 -0500

    cambios controller

--- backend/simrac/src/main/java/pe/sernanp/simrac/controller/VerificationMeanController.java
+++ backend/simrac/src/main/java/pe/sernanp/simrac/controller/VerificationMeanController.java
@@ -15,12 +15,11 @@ import pe.gisriv.entity.ResponseEntity;
 import pe.sernanp.simrac.model.VerificationMeanModel;
 import pe.sernanp.simrac.service.VerificationMeanService;
 
-
 @RestController
 @RequestMapping(value = "/verificationmean")
 public class VerificationMeanController extends BaseController<VerificationMeanModel, VerificationMeanService> {
 
-	@Autowired(required=true)
+	@Autowired
 	private VerificationMeanService _service;
 	
 	@RequestMapping(value = "/list")
