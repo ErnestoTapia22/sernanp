54ec6c61deaf82645d0bcb26373efd0d10d8640f
Angel Riv <angelrivr26@gmail.com>
2021-08-18 09:50:14 -0500

    conservation2

--- backend/simrac/src/main/java/pe/sernanp/simrac/model/ConservationAgreementModel.java
+++ backend/simrac/src/main/java/pe/sernanp/simrac/model/ConservationAgreementModel.java
@@ -8,9 +8,9 @@ import javax.persistence.Entity;
 public class ConservationAgreementModel extends BaseGeometryEntity {
 
 	private AgreementStateModel _agreementState;
-	private String _typeecosystemid;
+	
 	private Date _firm;
-	private int _validity;
+	/* private int _validity; */
 	private int _vigency;
 		
 	public AgreementStateModel getAgreementState() {
@@ -21,13 +21,6 @@ public class ConservationAgreementModel extends BaseGeometryEntity {
 		this._agreementState = Stateid;
 	}
 
-	public String getTypeecosystemid() {
-		return _typeecosystemid;
-	}
-
-	public void setTypeecosystemid(String Typeecosystemid) {
-		this._typeecosystemid = Typeecosystemid;
-	}
 
 	public Date getFirm() {
 		return _firm;
@@ -37,13 +30,13 @@ public class ConservationAgreementModel extends BaseGeometryEntity {
 		this._firm = firm;
 	}
 
-	public int getValidity() {
+	/*public int getValidity() {
 		return _validity;
 	}
 
 	public void setValidity(int validity) {
 		this._validity = validity;
-	}
+	} */
 	
 	public int getVigency() {
 		return _vigency;
