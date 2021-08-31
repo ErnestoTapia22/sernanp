0647cbb15d69b7f373047be8c704ada9db7b6722
Pier Rivera <pier.rivera9@gmail.com>
2021-08-31 18:26:24 -0500

    cambios user modules

--- backend/simrac/src/main/java/pe/sernanp/simrac/model/UserModel.java
+++ backend/simrac/src/main/java/pe/sernanp/simrac/model/UserModel.java
@@ -1,12 +1,14 @@
 package pe.sernanp.simrac.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 
 @Entity
 public class UserModel extends BaseModel {
 	
 	private int _system;
-	
+	private List<ModuleModel> _modules;
 		
 	public int getSystem () {
 		return _system;
@@ -15,5 +17,12 @@ public class UserModel extends BaseModel {
 	public void setSystem (int value) {
 		_system = value;
 	}
-		
+	
+	public List<ModuleModel>getModules () {
+		return _modules;
+	}
+	
+	public void setModules(List<ModuleModel> value) {
+		_modules = value;
+	}
 }
