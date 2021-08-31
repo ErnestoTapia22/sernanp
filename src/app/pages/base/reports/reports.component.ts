ba957fab82b1a7878eddc9261b8f09ae82718c0a
Ernesto Tapia <ernesto.tapia@touchmobile.pe>
2021-08-30 22:27:22 -0500

    add report

--- src/app/pages/base/reports/reports.component.ts
+++ src/app/pages/base/reports/reports.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.css']
+  styleUrls: ['./reports.component.css'],
 })
 export class ReportsComponent implements OnInit {
-
-  constructor() { }
+  url: string =
+    'https://i.postimg.cc/QCJnRkYt/Whats-App-Image-2021-08-30-at-6-31-19-PM.jpg';
+  urlSafe: SafeResourceUrl;
+  constructor(public sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
   }
-
 }
