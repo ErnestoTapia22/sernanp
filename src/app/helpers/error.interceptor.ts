2b29ec9b68f2e02b5812e03ccde103ad7200c264
Ernesto Tapia <ernesto.tapia@touchmobile.pe>
2021-09-02 17:07:37 -0500

    fix html

--- src/app/helpers/error.interceptor.ts
+++ src/app/helpers/error.interceptor.ts
@@ -15,15 +15,17 @@ export class ErrorInterceptor implements HttpInterceptor {
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    return next.handle(request).pipe(
-      catchError((err) => {
-        if ([401, 403].indexOf(err.status) !== -1) {
-          // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
-          this.authenticationService.logout();
-        }
-        const error = err.error.message || err.statusText;
-        return throwError(() => new Error(error));
-      })
-    );
+    return null;
+    // return next.handle(request).pipe(
+    //   catchError((err) => {
+
+    //     // if ([401, 403].indexOf(err.status) !== -1) {
+    //     //   // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
+    //     //   this.authenticationService.logout();
+    //     // }
+    //     // const error = err.error.message || err.statusText;
+    //     // return throwError(() => new Error(error));
+    //   })
+    // );
   }
 }
