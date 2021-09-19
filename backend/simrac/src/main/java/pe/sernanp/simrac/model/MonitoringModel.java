73d7152426f712c9c4d4c043a29a581bfc0d4f0c
Pier Rivera <pier.rivera9@gmail.com>
2021-09-18 19:38:20 -0500

    insertar monitoreo

--- backend/simrac/src/main/java/pe/sernanp/simrac/model/MonitoringModel.java
+++ backend/simrac/src/main/java/pe/sernanp/simrac/model/MonitoringModel.java
@@ -1,11 +1,17 @@
 package pe.sernanp.simrac.model;
 
+import java.util.List;
 import javax.persistence.Entity;
 
 @Entity
 public class MonitoringModel extends BaseModel {
 	
 	private String _comment;
+	private String _achievement;
+	private String _recommendation;
+	private String _evaluation;
+	private boolean _active;
+	private List<ActivityModel> _activities;
 
 	public String getComment() {
 		return _comment;
@@ -13,5 +19,44 @@ public class MonitoringModel extends BaseModel {
 
 	public void setComment(String comment) {
 		this._comment = comment;
+	}
+	
+	public String getAchievement() {
+		return _achievement;
+	}
+
+	public void setAchievement(String achievement) {
+		this._achievement = achievement;
+	}
+	
+	public String getRecommendation() {
+		return _recommendation;
+	}
+
+	public void setRecommendation(String recommendation) {
+		this._recommendation = recommendation;
+	}
+	public String getEvaluation() {
+		return _evaluation;
+	}
+
+	public void setEvaluation(String evaluation) {
+		this._evaluation = evaluation;
+	}
+	
+	public boolean getActive() {
+		return _active;
+	}
+
+	public void setActive(boolean active) {
+		this._active = active;
+	}
+	
+	public List<ActivityModel> getActivities() {
+		return _activities;
+	}
+		
+	public void setActivities(List<ActivityModel> activities) {
+		_activities = activities;		
 	}
 }
