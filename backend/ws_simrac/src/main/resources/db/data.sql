9c1bf3a8debdf02e219a87fc79ace2978e4b08a6
Pier Rivera <pier.rivera9@gmail.com>
2021-10-10 15:47:01 -0500

    conexion token

--- backend/ws_simrac/src/main/resources/db/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO simrac.t_estado_acuerdo(var_nom,txt_des,tsp_fec,bol_flg) VALUES ('Evaluación','','2021-08-30 00:00:00','t');
-INSERT INTO simrac.t_estado_acuerdo(var_nom,txt_des,tsp_fec,bol_flg) VALUES ('Diseño','','2021-08-30 00:00:00','t');
-INSERT INTO simrac.t_estado_acuerdo(var_nom,txt_des,tsp_fec,bol_flg) VALUES ('Firmado','','2021-08-30 00:00:00','t');
-INSERT INTO simrac.t_estado_acuerdo(var_nom,txt_des,tsp_fec,bol_flg) VALUES ('Implementación','','2021-08-30 00:00:00','t');
