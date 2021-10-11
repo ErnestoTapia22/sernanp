63ba2d1785f8bda24e736bab53a067c4010707cf
Angel Riv <angelrivr26@gmail.com>
2021-10-11 18:53:35 -0500

    delete

--- backend/ws_simrac/src/main/java/pe/sernanp/simrac/repository/ArticulateRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.sernanp.simrac.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.sernanp.simrac.model.ArticulateModel;
-
-public interface ArticulateRepository extends JpaRepository<ArticulateModel, Integer> {
-
-}
